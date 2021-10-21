4d40d7aa87da920a5a0e1fe241df57b627ceb368
Nick Castagnaro <ncastagnaro@wyattinc.com>
2021-10-21 08:48:11 -0400

    committing two files

---
+++ section23.js
@@ -0,0 +1,73 @@
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#message-show').classList.remove('hide');
+
+  document.querySelector('h1').innerHTML = message;
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+  event.preventDefault();
+
+  document.querySelector('#message-form').classList.add('hide');
+  document.querySelector('#link-form').classList.remove('hide');
+
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+//destructuring hash off of window.location. 
+//Same as window.location.hash 
+const { hash } = window.location;
+
+const message = atob(hash.replace('#',''));
+
+if(message){
+  document.querySelector('#message-form').classList.add('#hide');
+  document.querySelector('#message-show').classList.remove('#hide');
+
+  document.querySelector('h1').innerHTML = message; 
+
+}
+
+document.querySelector('form').addEventListener('submit', function(ev){
+ ev.preventDefault();
+
+ document.querySelector('#message-form').classList.add('hide');
+ document.querySelector('#link-form').classList.remove('hide');
+
+
+  const input = document.querySelector('#message-input');
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector('#link-input');
+  linkInput.value = `${window.location}#${encrypted}`;
+  linkInput.select();
+})
+
+
+*/
+
+
